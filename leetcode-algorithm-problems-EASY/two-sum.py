af6b0502edefbab49ce4c801d4635a2dc1e4cade
Nhi (Chelsea) Le <halannhile@gmail.com>
2020-08-04 16:03:57 +1000

    cleanup

--- leetcode-algorithm-problems-EASY/two-sum.py
+++ leetcode-algorithm-problems-EASY/two-sum.py
@@ -8,20 +8,13 @@ def twoSum(nums, target):
     :type target: int
     :rtype: List[int]
     """
-    h = {}
-    for i, num in enumerate(nums):
+    h = {} # dictionary: https://discuss.codecademy.com/t/i-dont-get-what-is-the-difference-between-and-in-python/46479
+    for i, num in enumerate(nums): # on enumerate(): https://book.pythontips.com/en/latest/enumerate.html
         n = target - num
         if n not in h:
             h[num] = i
         else:
             return [h[n], i]
-
-
-# testing:
-
-nums = [2,7,11,15]
-target = 9
-print(twoSum(nums, target))
 
 
 """
@@ -52,3 +45,25 @@ def twoSum(self, nums, target):
             seen[v] = i
         return []
 """
+
+
+
+"""
+alternative approach 3: 
+
+def twoSum(nums, target):
+    num_set = {}
+    for num_index, num in enumerate(nums):
+        if (target - num) in num_set:
+            return [num_set[target - num], num_index]
+        num_set[num] = num_index
+"""
+
+
+
+# testing:
+
+nums = [2,7,11,15]
+target = 9
+
+print(twoSum(nums, target))
