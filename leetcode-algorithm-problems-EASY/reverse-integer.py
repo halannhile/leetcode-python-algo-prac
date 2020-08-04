3e21d6178af975bd93e6d390d7f00556acf5cbb6
Nhi (Chelsea) Le <halannhile@gmail.com>
2020-08-04 19:12:38 +1000

    changed algo after testing

--- leetcode-algorithm-problems-EASY/reverse-integer.py
+++ leetcode-algorithm-problems-EASY/reverse-integer.py
@@ -3,28 +3,51 @@ https://leetcode.com/problems/reverse-integer/
 """
 
 
+
 def reverse(x):
-    """
-    :type x: int
-    :rtype: int
-    """
-    # return 0 if integers fall outside the 32-bit signed integer range: [−2**31,  2**31 − 1]
-    if x > 2 ** 31 - 1 or x < -2 ** 31:
+
+    # :type x: int
+    # :rtype: int
+
+    # return 0 if integer falls outside the 32-bit signed integer range: [−2**31,  2**31 − 1]
+    if x >= 2 ** 31 - 1 or x <= -2 ** 31:
         return 0
+
     else:
-        if x >= 0:
-            res = str(x)[::-1]  # reverse ordering of all digits
+        string = str(x)
+
+        if x >= 0:  # positive numbers
+            res = string[::-1]  # reverse ordering of all digits
+
+        else:  # negative numbers
+            digits = string[1:]  # extract digits part of string, i.e. without the - sign
+            reversed_digits = digits[::-1]
+            res = "-" + reversed_digits  # making it negative
+
+        # return 0 if integer falls ourside 32-bit signed integer range: [−2**31,  2**31 − 1]
+        if int(res) >= 2 ** 31 - 1 or int(res) <= -2 ** 31:
+            return 0
+
         else:
-            # making string positive, then reverse ordering of digits, then make it negative
-            res = -1 * int(str(x*-1)[::-1])
+            return int(res)
 
-            # alternative method:
 
-            # digits = string[1:] # extract digits part of string, i.e. without the - sign
-            # reversed_digits = digits[::-1]
-            # res = - reversed_digits # making it negative
+"""
+# alternative approach: 
 
-    return res
+class Solution:
+    def reverse(x):
+
+        s = str(abs(x))
+
+        reversed = int(s[::-1])
+
+        if reversed > 2147483647:
+            return 0
+
+        return reversed if x > 0 else (reversed * -1)
+"""
+
 
 # test:
 
